887087aaf9445ed57165502b065e7f7c78249c34
Jacob Thrasher <jthrasher0100@gmail.com>
2022-07-18 13:56:39 -0400

    Started discriminator updates

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 from scooby import in_ipython
+from sklearn.preprocessing import scale
 import torch
 from torch import batch_norm, nn
 from torchvision.models import inception_v3
@@ -82,6 +83,37 @@ class Identity(nn.Module):
     def forward(self, x):
         return x
 
+class ConvTranspose(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=5, stride=2, padding=0, bias=False, do_bn=True):
+        super().__init__()
+        self.do_bn = do_bn
+
+        self.convt = nn.ConvTranspose2d(in_channels, out_channels, stride=stride, kernel_size=kernel_size, padding=padding, bias=bias)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.convt(x)
+        if self.do_bn:
+            x = self.bn(x)
+        return self.relu(x)
+
+class Conv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=5, stride=2, padding=0, leaky_slope=0.2, bias=False, do_bn=False):
+        super().__init__()
+        self.do_bn = do_bn
+
+        self.conv = nn.Conv2d(in_channels, out_channels, stride=stride, kernel_size=kernel_size, padding=padding, bias=bias),
+        self.bn = nn.BatchNorm2d(out_channels),
+        self.lrelu = nn.LeakyReLU(leaky_slope, inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.do_bn:
+            x = self.bn(x)
+
+        return self.lrelu(x)
+
 #Network
 #TODO: Depth=5 fails, this is due to the scalar math I do, fix that
 class Generator(nn.Module):
@@ -93,7 +125,6 @@ class Generator(nn.Module):
                 
         super(Generator, self).__init__()
 
-        self.latent_dim = latent_dim
         self.f_maps = f_maps
         self.scale_init = scale_init
         self.alpha = 0
@@ -101,19 +132,17 @@ class Generator(nn.Module):
         self.depth = 0
 
         self.base = nn.Sequential()
-        self.base.add_module('conv1', conv_transpose(latent_dim, f_maps*scale_init, k_size=2, stride=1))
+        # self.base.add_module('conv_1', conv_transpose(latent_dim, f_maps*scale_init, k_size=2, stride=1))
+        self.base.add_module('conv_1', ConvTranspose(latent_dim, f_maps*scale_init, kernel_size=2, stride=1))
 
         self.old_head = nn.Sequential(OrderedDict([
             ('upsample', nn.Upsample(scale_factor=2, mode='nearest')),
             ('to_rgb', self.to_rgb(depth=0))
         ]))
 
-        # self.new_head = nn.Sequential(OrderedDict([
-        #     ('conv', conv_transpose(f_maps*scale_init, f_maps*int(scale_init/2), k_size=4, stride=2, padding=1)),
-        #     ('to_rgb', self.to_rgb(depth=1))
-        # ]))
+        s = int(self.scale_init / 2)
         self.new_head = nn.Sequential(OrderedDict([
-            ('conv', self.create_new_block(depth=1)),
+            ('conv', ConvTranspose(self.f_maps*self.scale_init, self.f_maps*s, kernel_size=4, stride=2, padding=1)),
             ('to_rgb', self.to_rgb(depth=1))
         ]))
 
@@ -123,13 +152,6 @@ class Generator(nn.Module):
         layers.append(nn.ConvTranspose2d(self.f_maps*s, 3, kernel_size=5, stride=1, padding=2))
         layers.append(nn.Tanh())
         return nn.Sequential(*layers)
-
-    def create_new_block(self, depth):
-        s = int(self.scale_init / (2 ** depth))
-        name = f'convt_{depth}'
-        layers = conv_transpose(self.f_maps*(s*2), self.f_maps*s, k_size=4, stride=2, padding=1)
-        return layers
-        # return layers, name
 
     def grow(self, depth):
         #Append new_head.conv to base
@@ -146,8 +168,9 @@ class Generator(nn.Module):
         self.old_head[-1].load_state_dict(module.state_dict())
 
         #Replace new head:
+        s = int(self.scale_init / (2 ** depth))
         self.new_head = nn.Sequential(OrderedDict([
-            ('conv', self.create_new_block(depth)),
+            ('conv', ConvTranspose(self.f_maps*(s*2), self.f_maps*s, kernel_size=4, stride=2, padding=1)),
             ('to_rgb', self.to_rgb(depth))
         ]))
 
@@ -161,7 +184,11 @@ class Generator(nn.Module):
         return x
 
 class Discriminator(nn.Module):
-    def __init__(self):
+    def __init__(self, 
+                f_maps=64,
+                scale_init=16,
+                a_rate=0.2,
+                channels = 3):
         super(Discriminator, self).__init__()
         # self.conv_0  = conv(3, F_MAPS, k_size=4, stride=2, padding=1, bn=False)
         # self.conv_1  = conv(F_MAPS, F_MAPS*2, k_size=4, stride=2, padding=1)
@@ -172,10 +199,26 @@ class Discriminator(nn.Module):
         # self.flatten = nn.Flatten()
         # self.dense   = nn.Linear(4*4*F_MAPS*8, 1)
 
+        self.f_maps = f_maps
+        self.scale_init = scale_init
+        self.a_rate = a_rate
+        self.alpha = 0
+        self.depth = 0
+
         init = conv(3, F_MAPS, k_size=4, stride=2, padding=1, bn=False)
         self.model = nn.Sequential()
         self.model.add_module('from_rgb', init)
         self.model.add_module('out', self.to_output(depth=0))
+
+        self.base = nn.Sequential()
+        self.base.add_module('conv_1', Conv(channels, self.f_maps, kernel_size=4, stride=2, padding=1, bn=False))
+
+        self.old_head = nn.Sequential(OrderedDict([
+            
+        ]))
+
+    def from_rgb(self, depth):
+        return
 
     def to_output(self, depth):
         s = depth ** 2
